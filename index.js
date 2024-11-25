11dc596ee4abde9ef2498324608c1a68f8829b1e
daps <ydapo50@gmail.com>
2024-11-25 04:12:11 +0100

    a basic design of my portfolio website

---
+++ index.js
@@ -0,0 +1,13 @@
+function remainder(a, b) {
+  return a % b;
+}
+
+console.log(remainder(3, 4));
+
+const firstIndex = (arr) => {
+  return arr[2];
+/// };
+
+const firstArray = [1, 2, 3, 4, 5, 6];
+
+console.log(firstIndex(firstArray));
